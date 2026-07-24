0ad40c1b26f6a37a2f1e9076ca01b28a4f106cb8
Avotrasina <favotrasina@gmail.com>
2026-07-24 18:54:16 +0300

    fix : Layout overflow issues

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <router-view></router-view>
+	<div class="h-full min-h-0 flex flex-col overflow-hidden">
+		<router-view></router-view>
+	</div>
 </template>
 
-
-<script setup>
-
-</script>
+<script setup></script>
 <style scoped></style>
